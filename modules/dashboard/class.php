110769a8d250975873918ee6d344632e17b6869a
Goga <glokhande@ebsolution.ca>
2026-04-05 16:16:54 +0530

    Keep selected dashboard date filter

--- modules/dashboard/class.php
+++ modules/dashboard/class.php
@@ -16,26 +16,6 @@ Class Dashboard{
         $fallbackNotice = '';
 
         $outboundSummary = $this->name->getOutboundSummary($rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-
-        if (intval($outboundSummary['total_calls'] ?? 0) === 0) {
-            foreach (['this_week', 'last_week', 'this_month', 'last_month'] as $fallbackKey) {
-                if ($fallbackKey === $selectedRange) {
-                    continue;
-                }
-
-                $candidateRange = $this->name->resolveDateRange($fallbackKey);
-                $candidateSummary = $this->name->getOutboundSummary($candidateRange['start_date'], $candidateRange['end_date'], $companyId);
-
-                if (intval($candidateSummary['total_calls'] ?? 0) > 0) {
-                    $fallbackNotice = 'No outbound activity was found for ' . $rangeInfo['label'] . '. Showing ' . $candidateRange['label'] . ' instead.';
-                    $rangeInfo = $candidateRange;
-                    $selectedRange = $candidateRange['key'];
-                    $outboundSummary = $candidateSummary;
-                    break;
-                }
-            }
-        }
-
         $statusBreakdown = $this->name->getCallStatusBreakdown($rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
         $campaignActivity = $this->name->getCampaignActivity($rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
         $agentPickupStats = $this->name->getAgentPickupAnalytics($rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
@@ -67,29 +47,6 @@ Class Dashboard{
         $point_5 = $this->name->pointfive("rate", "5", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
         $point_all = $this->name->totalcallpoint("rate", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
         $agentout = $this->name->averagescore("rate", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-
-        if (intval($point_all[0] ?? 0) === 0) {
-            foreach (['this_week', 'last_week', 'this_month', 'last_month'] as $fallbackKey) {
-                if ($fallbackKey === $selectedRange) {
-                    continue;
-                }
-
-                $candidateRange = $this->name->resolveDateRange($fallbackKey);
-                $candidateTotal = $this->name->totalcallpoint("rate", $candidateRange['start_date'], $candidateRange['end_date'], $companyId);
-
-                if (intval($candidateTotal[0] ?? 0) > 0) {
-                    $fallbackNotice = 'No rating data was found for ' . $rangeInfo['label'] . '. Showing ' . $candidateRange['label'] . ' instead.';
-                    $rangeInfo = $candidateRange;
-                    $selectedRange = $candidateRange['key'];
-                    $point_1 = $this->name->pointone("rate", "1", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-                    $point_3 = $this->name->pointthree("rate", "3", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-                    $point_5 = $this->name->pointfive("rate", "5", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-                    $point_all = $candidateTotal;
-                    $agentout = $this->name->averagescore("rate", $rangeInfo['start_date'], $rangeInfo['end_date'], $companyId);
-                    break;
-                }
-            }
-        }
         $rangeOptions = [
             'today' => 'Today',
             'this_week' => 'This Week',
